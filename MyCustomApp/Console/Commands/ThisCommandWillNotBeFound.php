aab92ae9be3de7c7044e8543a7e5781c190694a0
Trevor Fitzgerald <tfitzgerald@tectonics.com>
2017-08-31 12:00:43 -0400

    typo

--- MyCustomApp/Console/Commands/ThisCommandWillNotBeFound.php
+++ MyCustomApp/Console/Commands/ThisCommandWillNotBeFound.php
@@ -7,5 +7,5 @@ use Illuminate\Console\Command;
 class ThisCommandWillNotBeFound extends Command
 {
     protected $signature = 'command:not-found';
-    protected $description = 'This command will be NOT auto-registered, because it\'s in the MyCustomApp namespace.';
+    protected $description = 'This command will be NOT be auto-registered, because it\'s in the MyCustomApp namespace.';
 }
